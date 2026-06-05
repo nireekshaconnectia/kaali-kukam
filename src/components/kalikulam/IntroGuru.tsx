4d3f936ccaec572edd119a2dff307fb8b58e1d44
nireekshaconnectia <nireeksha.connectia@gmail.com>
2026-06-05 16:10:56 +0530

    fixed

--- src/components/kalikulam/IntroGuru.tsx
+++ src/components/kalikulam/IntroGuru.tsx
@@ -12,7 +12,7 @@ export function IntroGuru() {
   const isInView = useInView(sectionRef, { 
     amount: 0.1,
     once: false,  // Re-trigger every time
-    margin: "-380px 0px -380px 0px"
+    margin: "-180px 0px -180px 0px"
   });
 
   // Left to right animation for text
