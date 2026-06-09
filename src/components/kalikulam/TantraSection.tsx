6de2a8aff4c23bbead5036fb4adec395c5ffcaad
nireekshaconnectia <nireeksha.connectia@gmail.com>
2026-06-09 17:58:48 +0530

    fixed

--- src/components/kalikulam/TantraSection.tsx
+++ src/components/kalikulam/TantraSection.tsx
@@ -43,7 +43,7 @@ const items = [
 
 export function TantraSection() {
   return (
-    <section className="w-full py-16 px-10 md:px-61">
+    <section className="w-full  px-10 md:px-61 py-44">
       <div className="max-w-9xl mx-auto">
         <div className="mb-16 flex flex-col items-center text-center">
           <img
